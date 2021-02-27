1a46f4e9ec4a1b0e796b1206716b79fc35200248
Tyler Lowes <Tyler.Lowes@gmail.com>
2021-02-27 07:43:27 -0700

    Font and icons imort. Basic logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<section className='Game-container'>
+				<div className='logo-box'>
+					<div className='logo-back'>
+						<i className='fas fa-dice'></i>
+					</div>
+					<div className='logo-text'>Yahtzoo!</div>
+				</div>
+
+				<p>Hello</p>
+			</section>
+		</div>
+	);
 }
 
 export default App;
